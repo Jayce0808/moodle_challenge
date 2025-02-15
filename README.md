949e5633cf46611766a6062b3075ac573ab567a9
Jayce Birrell <66990367+Jayce0808@users.noreply.github.com>
2025-02-15 15:39:18 +1100

    Rearranged to do list, fixed typo and implemented step 1 and 2.
    Implemented --help options
    Implemented file validation

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 # To Do List
 
-1. Run from command line with options
-2. Implement --help option 
-3. Implement --dry_run option  
-2. Handle different options correctly 
-3. Read csv
-4. Validate email 
-5. Create DB programatically (implement --create_table option)
-6. Insert records 
+1. ~~Run from command line with options~~
+2. ~~Implement --help option~~ 
+3. Handle different options correctly 
+4. Read csv
+5. Validate email 
+6. Create DB programatically (implement --create_table option)
+7. Implement --dry_run option  
+8. Insert records 
+9. Implement testing? 
 
 # Run Instructions
 Run `php user_upload.php`
@@ -17,7 +18,7 @@ Run `php user_upload.php`
 # Command Line Directives 
  --file [csv file name] – this is the name of the CSV to be parsed.
  --create_table – this will cause the PostgreSQL users table to be built (and no further action will be taken).
- --dry_run – this will be used with the --fi le directive in case we want to run the script but not insert into the database. All other functions will be executed, but the database won't be altered.
+ --dry_run – this will be used with the --file directive in case we want to run the script but not insert into the database. All other functions will be executed, but the database won't be altered.
  -u – PostgreSQL username.
  -p – PostgreSQL password.
  -h – PostgreSQL host.
--- user_upload.php
+++ user_upload.php
@@ -1,8 +1,23 @@
 <?php 
 
 function main() {
-    $options = getopt("u:p:h", ["create_table", "dry_run", "file:", "help"]);
-    var_dump($options);
+    try {
+        $options = getopt("u:p:h", ["create_table", "dry_run", "file:", "help"]);
+        if (isset($options["help"])) {
+            displayCommandLineDirectives();
+        }
+        else if (isset($options["create_table"])) {
+            buildUsersTable();
+        }
+        else if (!isset($options["file"])) {
+            throw new Exception("A file must be provided!");
+        } else {
+            validateFile($options["file"]); //throws exception if invalid
+
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    } 
 }
 
 function readCSV() {
@@ -23,6 +38,26 @@ function buildUsersTable() {
 
 function connectToDB() {
 
+}
+
+function validateFile($filename) {
+    if (!file_exists($filename)) {
+        throw new Exception("Error: File '$filename' not found.\n");
+    } 
+    if (strtolower(pathinfo($filename, PATHINFO_EXTENSION)) !== 'csv') {
+        throw new Exception("Error: The provided file '$filename' is not a CSV.\n");
+    }
+    return true;
+}
+
+function displayCommandLineDirectives() {
+    echo "Usage: user_upload.php [options] [--] [args...]\n";
+    echo "--file [csv file name] - this is the name of the CSV to be parsed.\n";
+    echo "--create_table - this will cause the PostgreSQL users table to be built (and no further action will be taken).\n";
+    echo "--dry_run - this will be used with the --fi le directive in case we want to run the script but not insert into the database. All other functions will be executed, but the database won't be altered.\n";
+    echo "-u - PostgreSQL username.\n";
+    echo "-p - PostgreSQL password.\n";
+    echo "-h - PostgreSQL host.\n";
 }
 
 main();
